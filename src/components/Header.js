8f652dc23180683234d6c171215b1e2e8db1413b
Fred Garcia <Fred@Garcia.com>
2020-08-08 18:47:50 -0700

    back to 4 widgets

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,9 @@ export default function Header() {
                 <Link href="/dropdown" className="item">
                     Dropdown
                 </Link>
-                {/* <Link href="/translate" className="item">
+                <Link href="/translate" className="item">
                     Translate
-                </Link> */}
+                </Link>
             </div>
         </div>
     )
